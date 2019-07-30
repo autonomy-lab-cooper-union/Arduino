2cd6230431ef480c33671feff1f6e45cf2e5c0e5
EllaCM <43223216+EllaCM@users.noreply.github.com>
2019-07-31 03:07:29 +0800

    Arduino_Mega (encoder and imu)

---
+++ SPI-IMU (for edit).cpp
@@ -0,0 +1,102 @@
+#include <ros.h>
+#include <sensor_msgs/Imu.h>
+#include "SparkFunLSM6DS3.h"
+#include <Arduino.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/String.h>
+#include <tf/tf.h>
+#include <encoder.h>
+
+#define PI 3.1415
+//#define MOTORL
+//#define MOTORR
+#define PINA_L 2
+#define PINB_L 3
+#define PINZ_L 18
+#define PINA_R 19
+#define PINB_R 20
+#define PINZ_R 21
+
+ros::NodeHandle nhIMU;
+ros::NodeHandle nhEncoder;
+
+sensor_msgs::Imu imu_raw;
+ros::Publisher imu_pub("IMU", &imu_raw);
+
+LSM6DS3 myIMU(SPI_MODE, 10);
+
+void IMU_Setup();
+void IMU_loop();
+
+void setup()
+{
+	IMU_Setup();
+	Arduino_Encoder EncoderL(PINA_L, PINB_L, PINZ_L);
+	Arduino_Encoder EncoderR(PINA_R, PINB_R, PINZ_R);
+	nhIMU.initNode();
+	nhEncoder.initNode();
+	
+}
+
+void loop() 
+{
+	IMU_loop();
+	if (EncoderL.getPulse() % 1000 == 0)
+	{
+		nhEncoder.spinOnce();
+		EncoderL.updateSpeed();
+		EncoderL.encoder_b = EncoderL.encoderPos;
+
+	}
+	if (EncoderR.getPulse() % 1000 == 0)
+	{
+		nhEncoder.spinOnce();
+		EncoderR.updateSpeed();
+		EncoderR.encoder_b = EncoderR.encoderPos;
+
+	}
+	
+}
+
+
+
+
+
+void IMU_Setup()
+{
+	nhIMU.initNode();
+	nhIMU.advertise(imu_pub);
+	myIMU.begin();
+
+}
+
+
+void IMU_loop()
+{
+	imu_raw.header.stamp = nhIMU.now();
+	imu_raw.header.frame_id = "imu";
+	imu_raw.header.seq = 0;
+
+	imu_raw.linear_acceleration.x = float(myIMU.readFloatAccelX());
+	imu_raw.linear_acceleration.y = float(myIMU.readFloatAccelY());
+	imu_raw.linear_acceleration.z = float(myIMU.readFloatAccelZ());
+
+
+	imu_raw.angular_velocity.x = float(myIMU.readFloatGyroX()) - 3.451572;
+	imu_raw.angular_velocity.y = float(myIMU.readFloatGyroY()) + 9.933638;
+	imu_raw.angular_velocity.z = float(myIMU.readFloatGyroZ()) + 3.680171;
+
+	imu_raw.angular_velocity.x = imu_raw.angular_velocity.x * PI / 180;
+	imu_raw.angular_velocity.y = imu_raw.angular_velocity.y * PI / 180;
+	imu_raw.angular_velocity.z = imu_raw.angular_velocity.z * PI / 180;
+
+	imu_pub.publish(&imu_raw);
+
+	if (millis() % 10 == 0)
+	{
+		nhIMU.spinOnce();
+	}
+
+}
+
