01b6113a5fb658e8724697b09b398da7cb6b1c38
Kevin Jiang <kevinjiang11579@gmail.com>
2019-10-05 15:08:12 -0400

    Cleaning out unnecessary folders and files

--- Arduino_Mega/Arduino_Mega.ino
+++ Arduino_Mega/Arduino_Mega.ino
@@ -1,142 +1,259 @@
-#include <encoder.h>
 
-//#include <ros.h>
-#include <sensor_msgs/Imu.h>
-#include "SparkFunLSM6DS3.h"
-//#include <Arduino.h>
-//#include <std_msgs/Int32.h>
-//#include <std_msgs/Int8.h>
-//#include <std_msgs/String.h>
-#include <tf/tf.h>
+#include <PID_v1.h>
+//input:velocity from computer sent through ros
+//output:pwm to motor controller
+#include <ros.h>
+#include <Arduino.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Int32.h>
+
+ros::NodeHandle  nh;
+
+//*** Below are the pin configurations for both encoders. A is Green wire, B is White wire, Z is Yellow wire, Red and Black are 5V VCC and GND.
+#define encoder0PinA 2
+#define encoder0PinB 3
+#define encoder0PinZ 18
+#define encoder1PinA 19
+#define encoder1PinB 20
+#define encoder1PinZ 21
+//*** Pin configurations for the motor controllers. Accel means rear motor controller, Direc means front motor controller
+#define accelPWM 9
+#define accelDIR 8
+#define direcPWM 6
+#define direcDIR 7
+
+//*** Parameters for Acceleration PID
+#define kpA 3
+#define kiA 0
+#define kdA 0
+
+//*** Parameters for Direction PID
+#define kpD 3
+#define kiD 0.3
+#define kdD 0
+
+//output conencts to potentiometer of the motor controller's arduino?
 
 
-//#define PI 3.1415
-#define MOTORL 4
-#define MOTORR 6
-#define PINA_L 2
-#define PINB_L 3
-#define PINZ_L 18
-#define PINA_R 19
-#define PINB_R 20
-#define PINZ_R 21
+double vCurrent, vTarget; //current and target speed variables
+double target_angle, front_angle; // targe
+double accelpwm; //variable for pwm value for acceleration
+double direcpwm; //variable for pwn value for direction
 
-ros::NodeHandle nhIMU;
-ros::NodeHandle nhEncoder;
+//unsigned volatile short pulse0;
+//unsigned volatile short pulse1;
 
-sensor_msgs::Imu imu_raw;
-ros::Publisher imu_pub("IMU", &imu_raw);
+PID accelPID (&vCurrent, &accelpwm, &vTarget, kpA, kiA, kdA, DIRECT);
+PID direcPID (&front_angle, &direcpwm, &target_angle, kpD, kiD, kdD, DIRECT);
 
-LSM6DS3 myIMU(SPI_MODE, 10);
+volatile int encoder0Pos = 0;
+//int encoder0; //encoder0Pos before for testing CCW OR CW
+//int encoder0_b; //mark the encoder0Pos when pulses change
 
-void IMU_Setup();
-void IMU_loop();
-Arduino_Encoder EncoderL(MOTORL, PINA_L, PINB_L, PINZ_L);
-Arduino_Encoder EncoderR(MOTORR, PINA_R, PINB_R, PINZ_R);
+volatile int encoder1Pos = 0;
+//int encoder1; //encoder1Pos before for testing CCW OR CW
+//int encoder1_b; //mark the encoder0Pos when pulse changes (this means that a revolution for the rear wheel has passed)
 
-void doEncoderL() {
-  EncoderL.pulse++;
-  EncoderL.encoder = EncoderL.encoderPos;
-  if (digitalRead(EncoderL.encoderPinZ) == 1) {
-    EncoderL.encoderPos = 0;
-  }
-  else if (digitalRead(EncoderL.encoderPinA) == digitalRead(EncoderL.encoderPinB)) {
-    EncoderL.encoderPos++;
-  }
-  else {
-    EncoderL.encoderPos--;
-  }
 
-  if (abs(EncoderL.encoder) > abs(EncoderL.encoderPos)) {
-    EncoderL.pulse = 0;
-  }
-  Serial.println(EncoderL.encoderPos, DEC);
+//std_msgs::String str_msg;
+int left_encoder;
+int right_encoder;
+double left_speed;
+double right_speed;
+
+
+//fetch current left wheel speed from ROS
+void currentSpeed_left(const std_msgs::Float64 &val) {
+  left_speed = val.data;
+} 
+
+//fetch current right wheel speed from ROS
+void currentSpeed_right(const std_msgs::Float64 &val) {
+  right_speed = val.data;
 }
 
-void doEncoderR() {
-  EncoderR.pulse++;
-  EncoderR.encoder = EncoderR.encoderPos;
-  if (digitalRead(EncoderR.encoderPinZ) == 1) {
-    EncoderR.encoderPos = 0;
-  }
-  else if (digitalRead(EncoderR.encoderPinA) == digitalRead(EncoderR.encoderPinB)) {
-    EncoderR.encoderPos++;
-  }
-  else {
-    EncoderR.encoderPos--;
-  }
-
-  if (abs(EncoderR.encoder) > abs(EncoderR.encoderPos)) {
-    EncoderR.pulse = 0;
-  }
-  Serial.println(EncoderR.encoderPos, DEC);
+//fetch current RAW data from absolute encoder from ROS and convert it to radians
+void currentRadian_front(const std_msgs::Int32 &val) {
+  front_angle = ((double)val.data)*2*PI/1024;
 }
 
-void setup()
-{
-  IMU_Setup();
+void targetSpeed(const std_msgs::Float64 &val) {
+  vTarget = val.data;
+}
+
+void targetRadian(const std_msgs::Float64 &val) {
+  target_angle = val.data;
+}
+
+
+//"" is the topic name, pc...--name of subscriber
+//ros::Subscriber<std_msgs::Int32> pc_vTarget("motor_vTarget", &updateVTarget);
+ros::Publisher lwheel_tick("lwheel", left_encoder);
+ros::Publisher rwheel_tick("rwheel", right_encoder);
+ros::Subscriber<std_msgs::Float64> lwheel_speed("lwheel_speed", &currentSpeed_left);  //current left wheel speed calculated by computer
+ros::Subscriber<std_msgs::Float64> rwheel_speed("rwheel_speed", &currentSpeed_right);  //current right wheel speed calculated by computer
+ros::Subscriber<std_msgs::Int32> fwheel_tick("fwheel_tick", &currentRadian_front); //current RAW data from absolute encoder in front
+ros::Subscriber<std_msgs::Float64> control_speed("control_speed", &targetSpeed); //target speed from computer
+ros::Subscriber<std_msgs::Float64> control_steering("control_steering", &targetRadian); //target radians from computer
+
+void setup() {
+  nh.initNode();  
+  nh.subscribe("lwheel_speed");
+  nh.subscribe("rhweel_speed");
+  nh.subscribe(fwheel_tick);
+  nh.advertise(lwheel_tick);
+  nh.advertise(rwheel_tick);
+  nh.subscribe(control_speed);
+  nh.subscribe(control_steering);
   
-  nhIMU.initNode();
-  nhEncoder.initNode();
+  
+  pinMode(encoder0PinA, INPUT);
+  digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
+  pinMode(encoder0PinB, INPUT);
+  digitalWrite(encoder0PinB, HIGH);       // turn on pull-up resistor
+  pinMode(encoder0PinZ, INPUT);
+  digitalWrite(encoder0PinZ, HIGH);       // turn on pull-up resistor
+  pinMode(encoder1PinA, INPUT);
+  digitalWrite(encoder1PinA, HIGH);       // turn on pull-up resistor
+  pinMode(encoder1PinB, INPUT);
+  digitalWrite(encoder1PinB, HIGH);       // turn on pull-up resistor
+  pinMode(encoder1PinZ, INPUT);
+  digitalWrite(encoder1PinZ, HIGH);       // turn on pull-up resistor
+  pinMode(accelPWM, OUTPUT);
+  pinMode(direcPWM, OUTPUT);
+  pinMode(accelDIR, OUTPUT);
+  pinMode(direcDIR, OUTPUT);
+    
+  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoderA(0), CHANGE);  // Output channel A from encoder 0 -> interrupt pin 2
+  attachInterrupt(digitalPinToInterrupt(encoder0PinB), doEncoderB(0), CHANGE);  // Output channel B from encoder 0 -> interrupt pin 3
+  attachInterrupt(digitalPinToInterrupt(encoder0PinZ), doEncoderC(0), RISING); // Output channel C from encoder 0 -> interrupt pin 18       
+  attachInterrupt(digitalPinToInterrupt(encoder1PinA), doEncoderA(1), CHANGE);  // Output channel A from encoder 1 -> interrupt pin 2
+  attachInterrupt(digitalPinToInterrupt(encoder1PinB), doEncoderB(1), CHANGE);  // Output channel B from encoder 1 -> interrupt pin 3
+  attachInterrupt(digitalPinToInterrupt(encoder1PinZ), doEncoderC(1), RISING); // Output channel C from encoder 1 -> interrupt pin 18          
+  
+  //accept setPoint value from commands of PC
+  //will it get update again if put in the setup() function?
+  myPID.SetMode(AUTOMATIC);
 
-  attachInterrupt(digitalPinToInterrupt(EncoderL.encoderPinA), doEncoderL, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(EncoderL.encoderPinZ), doEncoderL, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(EncoderR.encoderPinA), doEncoderR, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(EncoderR.encoderPinZ), doEncoderR, CHANGE);
+  
 }
 
-void loop() 
-{
-  IMU_loop();
-  if (EncoderL.getPulse() % 1000 == 0)
-  {
-    nhEncoder.spinOnce();
-    EncoderL.updateSpeed();
-    EncoderL.encoder_b = EncoderL.encoderPos;
+void loop() {
+  left_encoder = encoder0Pos;
+  right_encoder = encoder1Pos;
+  lwheel_tick.publish(&left_encoder);
+  rwheel_tick.publish(&right_encoder);
+  vCurrent = (left_speed + right_speed) / 2;
+  updateVelocity();
+}
 
+void doEncoderA(bool encoderNum) {
+  if (encoderNum == 0)
+  { 
+    encoder0 = encoder0Pos;
+  /*  if (digitalRead(encoder0PinA) == HIGH){  //Commented out because we no longer need to reset the pulse, computer takes care of speed calc
+      pulse0++;
+    } */
+    if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)){
+      encoder0Pos--;
+    }
+    else { 
+      encoder0Pos++;
+    }
+/*    if (abs(encoder0) > abs(encoder0Pos)){  //Commented out because we no longer need to reset the pulse, computer takes care of speed calc
+      pulse0 = 0;
+    }*/
   }
-  if (EncoderR.getPulse() % 1000 == 0)
+  else
   {
-    nhEncoder.spinOnce();
-    EncoderR.updateSpeed();
-    EncoderR.encoder_b = EncoderR.encoderPos;
-
-  }  
-}
-
-
-
-
-
-void IMU_Setup()
-{
-  nhIMU.initNode();
-  nhIMU.advertise(imu_pub);
-  myIMU.begin();
-
-}
-
-void IMU_loop()
-{
-  imu_raw.header.frame_id = "imu";
-  imu_raw.header.seq = 0;
-
-  imu_raw.linear_acceleration.x = float(myIMU.readFloatAccelX());
-  imu_raw.linear_acceleration.y = float(myIMU.readFloatAccelY());
-  imu_raw.linear_acceleration.z = float(myIMU.readFloatAccelZ());
-
-
-  imu_raw.angular_velocity.x = float(myIMU.readFloatGyroX()) - 3.451572;
-  imu_raw.angular_velocity.y = float(myIMU.readFloatGyroY()) + 9.933638;
-  imu_raw.angular_velocity.z = float(myIMU.readFloatGyroZ()) + 3.680171;
-
-  imu_raw.angular_velocity.x = imu_raw.angular_velocity.x * PI / 180;
-  imu_raw.angular_velocity.y = imu_raw.angular_velocity.y * PI / 180;
-  imu_raw.angular_velocity.z = imu_raw.angular_velocity.z * PI / 180;
-
-  imu_pub.publish(&imu_raw);
-
-  if (millis() % 10 == 0)
-  {
-    nhIMU.spinOnce();
+    encoder1 = encoder1Pos;
+    /*if (digitalRead(encoder1PinA) == HIGH){  //Commented out because we no longer need to reset the pulse, computer takes care of speed calc
+      pulse1++;
+    } */
+    if (digitalRead(encoder1PinA) == digitalRead(encoder1PinB)){
+      encoder1Pos--;
+    }
+    else { 
+      encoder1Pos++;
+    }
+   /* if (abs(encoder1) > abs(encoder1Pos)){  //Commented out because we no longer need to reset the pulse, computer takes care of speed calc
+      pulse1 = 0;
+    } */
   }
+}
+
+void doEncoderB(bool encoderNum) {
+  if (encoderNum == 0){
+   // encoder0 = encoder0Pos;
+    if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)){
+      encoder0Pos++;
+    }
+    else {
+      encoder0Pos--;
+    }
+   /* if (abs(encoder0) > abs(encoder0Pos)){  //Commented out because we no longer need to reset the pulse, computer takes care of speed calc
+      pulse0 = 0;
+    }  */
+  }
+  else {
+   // encoder1 = encoder1Pos;
+    if (digitalRead(encoder1PinA) == digitalRead(encoder1PinB)){
+      encoder1Pos++;
+    }
+    else {
+      encoder1Pos--;
+    }
+   /* if (abs(encoder1) > abs(encoder1Pos)){  //Commented out because we no longer need to reset the pulse, computer takes care of speed calc
+      pulse1 = 0;
+    }  */
+  }
+
+}
+
+void doEncoderC(bool encoderNum) {
+  if (encoderNum == 0){
+    encoder0Pos = 0;
+  }
+  else{
+    encoder1Pos = 0;
+  }
+}
+
+
+
+void updateVelocity(){
+  
+    accelPID.Compute();
+    direcPID.Compute();
+    updateAccel(accelpwm, direcpwm);
+   /* Seril.println(pwm, DEC);
+    analogWrite(PWM, pwm);
+    String str_pwm = String(pwm);
+    int str_pwm_length = str_pwm.length() + 1;
+    char pwm_str_array[str_pwm_length];
+    str_pwm.toCharArray(pwm_str_array, str_pwm_length);
+    str_msg.data = pwm_str_array;
+    pub_vTarget.publish(&str_msg); */
+}
+
+//Remember to check whether the directions are correct
+void updateAccel(int accelpwm, int direcpwm) {
+  if(accelpwm <= 0) {
+    digitalWrite(accelDIR, HIGH);
+  }
+  else {
+    digitalWrite(accelDIR, LOW);
+  }
+  
+  if(direcpwm <= 0) {
+    digitalWrite(direcDIR, HIGH);
+  }
+  else {
+    digitalWrite(direcDIR, LOW);
+  }
+  
+  analogWrite(accelPWM, abs(accelpwm));
+  
+  analogWrite(direcPWM, abs(direcpwm));
+  
 }
